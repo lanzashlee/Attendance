672ceefc06f22037a7bd9a9ee1edbd89b839f289
Lanz Ashlee Ricamara <111546362+lanzashlee@users.noreply.github.com>
2023-05-25 15:17:59 +0800

    Create U2_SpyPlane.java

---
+++ May_25/U2_SpyPlane.java
@@ -0,0 +1,16 @@
+package May25;
+
+public class U2_SpyPlane extends Vehicle {
+		
+		 int WingSpan = 103;
+			
+			void Fly() {
+				System.out.println("The Plane is flying");
+			}
+			
+			
+			@Override
+			void Stop() {
+				super.Stop();
+			}
+}
