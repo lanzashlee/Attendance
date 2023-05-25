d9ebf64dd454947abb4382bf605a5e765ca5b7e6
Lanz Ashlee Ricamara <111546362+lanzashlee@users.noreply.github.com>
2023-05-25 15:19:52 +0800

    Create FandangoYacht.java

---
+++ May_25/FandangoYacht.java
@@ -0,0 +1,14 @@
+package May25;
+
+public class FandangoYacht extends Vehicle {
+	String MainSailColor = "";
+	
+	void Float() {
+		System.out.println("The boat is sailing");
+	}
+	
+	@Override
+	void Stop() {
+		super.Stop();
+	}
+}
