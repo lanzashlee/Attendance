b9afe4e2970418c78b2ad4dda22927d7d3b0a7f0
Lanz Ashlee Ricamara <111546362+lanzashlee@users.noreply.github.com>
2023-05-25 15:18:49 +0800

    Create Toyota_VIOS.java

---
+++ May_25/Toyota_VIOS.java
@@ -0,0 +1,15 @@
+package May25;
+
+public class Toyota_VIOS extends Vehicle{
+	 String TireType = "Performance Tire";
+		
+		void Drive() {
+			System.out.println("The Car is accelerating");
+		}
+		
+
+		@Override
+		void Stop() {
+			super.Stop();
+		}
+}
