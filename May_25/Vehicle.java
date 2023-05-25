c4b3d77919acc5c4d17bf980d6e8bc39745f672f
Lanz Ashlee Ricamara <111546362+lanzashlee@users.noreply.github.com>
2023-05-25 15:17:16 +0800

    Create Vehicle.java

---
+++ May_25/Vehicle.java
@@ -0,0 +1,20 @@
+package May25;
+public class Vehicle {
+	
+	int ToyotaSpeed = 60;
+	String ToyotaColor = "Black";
+	String ToyotaPrice = "₱ 1,055,000";
+	
+	int PlaneSpeed = 600;
+	String PlaneColor = "White";
+	String PlanePrice = "$20,000,000";
+	
+	int YachtSpeed = 48;
+	String YachtColor = "Blue";
+	String YachtPrice = "$500,000";
+	
+
+	void Stop(){
+		System.out.println("The Vehicle has stopped");
+	}
+}
